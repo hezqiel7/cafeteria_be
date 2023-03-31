78881b698fb78ddfe0d63cdef029dcff8eac9846
Ezequiel Aguilera <ezequielagui102@gmail.com>
2023-03-31 13:51:55 -0400

    Agregar permisos de administrador a usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from django.contrib.auth.models import User
 from .serializers import UsuarioSerializer
+from rest_framework.permissions import IsAdminUser
 
 class UsuariosViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UsuarioSerializer
+    permission_classes = [IsAdminUser]
     
     class Meta:
         model=User
