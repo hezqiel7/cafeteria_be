304f01aa46fcd3cbc8a7a6e5adc2f5b245130ff9
Ezequiel Aguilera <ezequielagui102@gmail.com>
2023-05-06 20:45:42 -0400

    cambios en el requirements.txt

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,11 +1,13 @@
 from djongo import models
 from django.utils import timezone
 
+
 class Pedido(models.Model):
     id = models.IntegerField(primary_key=True)
     mesa = models.IntegerField("Nro de mesa")
     listo = models.BooleanField("Listo para servir", default=False)
-    fecha_pedido = models.DateTimeField("Fecha del pedido", default=timezone.now)
+    fecha_pedido = models.DateTimeField(
+        "Fecha del pedido", default=timezone.now)
     lista_productos = models.JSONField("Lista de productos")
     total_precio = models.IntegerField("Precio total", null=True)
     nombre_cliente = models.TextField("Nombre del cliente")
--- pedidos/serializers.py
+++ pedidos/serializers.py
@@ -2,31 +2,36 @@ from rest_framework import serializers
 from .models import Pedido
 from productos.models import Producto
 
+
 class ProductoCantidadSerializer(serializers.Serializer):
     producto_id = serializers.IntegerField()
     cantidad = serializers.IntegerField()
 
+
 class PedidoSerializer(serializers.ModelSerializer):
-    
+
     lista_productos = ProductoCantidadSerializer(many=True)
-    
+
     class Meta:
         model = Pedido
-        fields = ['id', 'mesa', 'listo', 'fecha_pedido', 'lista_productos', 'total_precio', 'nombre_cliente']
-        
+        fields = '__all__'
+        # fields = [Pedido.pk, 'mesa', 'listo', 'fecha_pedido', 'lista_productos', 'total_precio', 'nombre_cliente']
+
     def create(self, validated_data):
         lista_productos = validated_data.get('lista_productos')
         total = 0
         for producto in lista_productos:
-            total += Producto.objects.get(pk=producto['producto_id']).precio * producto['cantidad']
+            total += Producto.objects.get(
+                pk=producto['producto_id']).precio * producto['cantidad']
         validated_data['total_precio'] = total
         return super().create(validated_data)
-    
+
     def update(self, instance, validated_data):
         lista_productos = validated_data.get('lista_productos')
         total = 0
         for producto in lista_productos:
-            total += Producto.objects.get(pk=producto['producto_id']).precio * producto['cantidad']
+            total += Producto.objects.get(
+                pk=producto['producto_id']).precio * producto['cantidad']
         validated_data['total_precio'] = total
         return super().update(instance, validated_data)
 
@@ -34,4 +39,3 @@ class PedidoSerializer(serializers.ModelSerializer):
     #     representation = super().to_representation(instance)
     #     representation['lista_productos'] = json.loads(representation['lista_productos'])
     #     return representation
-    
