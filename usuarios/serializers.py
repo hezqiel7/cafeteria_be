f1360c0ad6e94c8c49cb83e15553da885ca94cba
Ezequiel Aguilera <ezequielagui102@gmail.com>
2023-05-14 18:52:18 -0400

    agregar get para listar los grupos de un user

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -1,28 +1,36 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.hashers import make_password, check_password
 
+
 class UsuarioSerializer(serializers.ModelSerializer):
-    
+
     def create(self, validated_data):
         # Encriptamos la contraseña antes de guardarla en la base de datos
         validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
-    
+
     def update(self, instance, validated_data):
         # Si la contraseña se modificó
         if instance.password != validated_data.get('password'):
             # Encriptar la nueva contraseña
-            validated_data['password'] = make_password(validated_data['password'])
+            validated_data['password'] = make_password(
+                validated_data['password'])
         return super().update(instance, validated_data)
 
     class Meta:
-        model=User
-        fields=['id',
-                'username',
-                'password',
-                'first_name',
-                'last_name',
-                'email',
-                'is_staff'
-                ]
+        model = User
+        fields = ['id',
+                  'username',
+                  'password',
+                  'first_name',
+                  'last_name',
+                  'email',
+                  'is_staff'
+                  ]
+
+
+class GrupoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['id', 'name']
--- usuarios/views.py
+++ usuarios/views.py
@@ -2,15 +2,26 @@ from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from django.contrib.auth.models import User
-from .serializers import UsuarioSerializer
+from .serializers import UsuarioSerializer, GrupoSerializer
 
 
 class UsuariosViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UsuarioSerializer
     permission_classes = [IsAdminUser]
-    
+
     class Meta:
-        model=User
-        
+        model = User
+
+    @action(detail=True, methods=['get'])
+    def grupos(self, request, pk=None):
+        id_usuario = self.kwargs['pk']
+        try:
+            usuario = User.objects.get(pk=id_usuario)
+            grupos = usuario.groups.all()
+            return Response(data=GrupoSerializer(grupos, many=True).data, status=200)
+        except Exception as e:
+            print(e)
+            return Response(None, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
